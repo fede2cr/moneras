d86a67a3da23b195475289ded7e099d21af71fd2
Alvaro Figueroa <alvaro.figueroa@microsoft.com>
2022-04-12 13:30:53 -0600

    Adding pylinted version, split with secrets.py

---
+++ Testing/Manual_Testing/secrets.py
@@ -0,0 +1,7 @@
+TRAINING_ENDPOINT = "https://copy-paste-endpoint.cognitiveservices.azure.com/"
+TRAINING_KEY = "copy-paste-key"
+PREDICTION_ENDPOINT = "https://copy-paste-prediction-endpoint.cognitiveservices.azure.com/"
+PREDICTION_KEY = "copy-paste-prediction-key"
+# Resource ID split into two lines
+PREDICTION_RESOURCE_ID = "/subscriptions/123-456-789/" + \
+    "resourceGroups/Monero/providers/Microsoft.CognitiveServices/accounts/Moneras-Prediction"
