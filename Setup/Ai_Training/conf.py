4af5facaac5f10d2fcda17d5a8c689111cf63bd5
Alvaro Figueroa <alvaro.figueroa@microsoft.com>
2022-04-12 19:34:02 -0600

    Integrating inat stuff and azure stuff. inat ready

--- Setup/Ai_Training/conf.py
+++ Setup/Ai_Training/conf.py
@@ -1,5 +1,15 @@
 '''
-File for configuring azure password for moneras project
+Configuration file for the project. Add here the information for the species
+that the cameras are going to focus on.
+'''
+SPECIES = ['Alouatta palliata', 'Cebus imitator']
+
+# iNaturalist configuration
+INAT_USER="fede2cr"
+
+'''
+Grab this information from the Azure Portal. You should have two resources,
+the trainig resource, and the prediction resource.
 '''
 TRAINING_ENDPOINT = "https://copy-paste-endpoint.cognitiveservices.azure.com/"
 TRAINING_KEY = "copy-paste-key"
@@ -8,3 +18,6 @@ PREDICTION_KEY = "copy-paste-prediction-key"
 # Resource ID split into two lines
 PREDICTION_RESOURCE_ID = "/subscriptions/123-456-789/" + \
     "resourceGroups/Monero/providers/Microsoft.CognitiveServices/accounts/Moneras-Prediction"
+
+# General app config, no need to edit here
+DIR_PATH = "Training_Images/"
--- Setup/Ai_Training/inat_image_downloader.py
+++ Setup/Ai_Training/inat_image_downloader.py
@@ -7,30 +7,33 @@ Mejora: Se puede reutilizar el código para descargar todas las especies vistas 
 '''
 
 import os
+import conf
 from pyinaturalist import get_observations
 import requests
 
-USER='fede2cr'
-#SPECIES='Alouatta palliata'
-SPECIES='Cebus imitator'
+def download_species(SPECIES, INAT_USER):
+    DIR = os.path.join(conf.DIR_PATH, SPECIES)
+    IS_EXIST = os.path.exists(DIR)
+    if not IS_EXIST:
+        os.makedirs(DIR)
+    
+    observations = get_observations(user_id=INAT_USER, page='all')
+    
+    for obs in observations['results']:
+        if obs["taxon"]["name"] == SPECIES:
+            for i in range(0, len(obs["observation_photos"])):
+                if obs["observation_photos"][i]["photo"]["license_code"] == "cc-by":
+                    #print(obs["observation_photos"][i]["photo"])
+                    ORIG_URL = ("https://inaturalist-open-data.s3.amazonaws.com/photos/"
+                        + str(obs["observation_photos"][i]["photo"]["id"]) + "/original.jpeg")
+                    print("Downloading: ", ORIG_URL)
+                    r = requests.get(ORIG_URL, allow_redirects=True)
+                    with open(DIR + "/" + str(obs["observation_photos"][i]["photo"]["id"])
+                        + '.jpeg', 'wb') as f:
+                        f.write(r.content)
+            print("=" * 91)
+ 
+for species in conf.SPECIES:
+    print(f"Downloading species: {species}")
+    download_species(species, conf.INAT_USER)
 
-IS_EXIST = os.path.exists(SPECIES)
-if not IS_EXIST:
-    os.makedirs(SPECIES)
-
-observations = get_observations(user_id=USER, page='all')
-
-for obs in observations['results']:
-    if obs["taxon"]["name"] == SPECIES:
-        for i in range(0, len(obs["observation_photos"])):
-            if obs["observation_photos"][i]["photo"]["license_code"] == "cc-by":
-                #print(obs["observation_photos"][i]["photo"])
-                ORIG_URL = ("https://inaturalist-open-data.s3.amazonaws.com/photos/"
-                    + str(obs["observation_photos"][i]["photo"]["id"]) + "/original.jpeg")
-                print("Downloading: ", ORIG_URL)
-                r = requests.get(ORIG_URL, allow_redirects=True)
-                with open(SPECIES + "/" + str(obs["observation_photos"][i]["photo"]["id"])
-                    + '.jpeg', 'wb') as f:
-                    f.write(r.content)
-    #print(obs.keys())
-        print("=" * 80)
