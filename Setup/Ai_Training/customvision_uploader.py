ce63d84c8e8f9eb8ad369f0c85c2df0d4ab15091
Alvaro Figueroa <alvaro.figueroa@microsoft.com>
2022-04-15 12:05:20 -0600

    Code formatting with black

--- Setup/Ai_Training/customvision_uploader.py
+++ Setup/Ai_Training/customvision_uploader.py
@@ -1,40 +1,53 @@
-'''
+"""
 This script is used to create in one step all of the training tags for Customvision.ai
 as well as to upload the downloaded images, train the system to recognize this images,
 and test the trained model.
 When it finished, it will print the porcentage of confidance it has in the test
 identification.
-'''
+"""
 
 import os
 import time
 import sys
-from azure.cognitiveservices.vision.customvision.training import CustomVisionTrainingClient
-from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
-from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateEntry
-from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch
+from azure.cognitiveservices.vision.customvision.training import (
+    CustomVisionTrainingClient,
+)
+from azure.cognitiveservices.vision.customvision.prediction import (
+    CustomVisionPredictionClient,
+)
+from azure.cognitiveservices.vision.customvision.training.models import (
+    ImageFileCreateEntry,
+)
+from azure.cognitiveservices.vision.customvision.training.models import (
+    ImageFileCreateBatch,
+)
 from msrest.authentication import ApiKeyCredentials
 import conf
 
 credentials = ApiKeyCredentials(in_headers={"Training-key": conf.TRAINING_KEY})
 trainer = CustomVisionTrainingClient(conf.TRAINING_ENDPOINT, credentials)
-prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": conf.PREDICTION_KEY})
-predictor = CustomVisionPredictionClient(conf.PREDICTION_ENDPOINT, prediction_credentials)
+prediction_credentials = ApiKeyCredentials(
+    in_headers={"Prediction-key": conf.PREDICTION_KEY}
+)
+predictor = CustomVisionPredictionClient(
+    conf.PREDICTION_ENDPOINT, prediction_credentials
+)
 
 PUBLISH_ITERATION_NAME = "classifyModel"
 
-print ("Creating project...")
+print("Creating project...")
 PROJECT_NAME = "Moneras"
 project = trainer.create_project(PROJECT_NAME)
 
+
 def tag_species(species):
-    '''
+    """
     Defines a CustomVision.ai tag with an inat species, tags the images
     and uploads the current tagged images
-    '''
+    """
     tag = trainer.create_tag(project.id, species)
 
-    base_image_location = os.path.join (os.path.dirname(__file__), conf.DIR_PATH)
+    base_image_location = os.path.join(os.path.dirname(__file__), conf.DIR_PATH)
 
     print("Adding images...")
     image_list = []
@@ -42,41 +55,55 @@ def tag_species(species):
     file_list = next(os.walk(base_image_location + species), (None, None, []))[2]
 
     for file_name in file_list:
-        with open(os.path.join (base_image_location, species,
-            file_name), "rb") as tag_image_contents:
-            image_list.append(ImageFileCreateEntry(name=file_name,
-                contents=tag_image_contents.read(), tag_ids=[tag.id]))
+        with open(
+            os.path.join(base_image_location, species, file_name), "rb"
+        ) as tag_image_contents:
+            image_list.append(
+                ImageFileCreateEntry(
+                    name=file_name, contents=tag_image_contents.read(), tag_ids=[tag.id]
+                )
+            )
 
-    upload_result = trainer.create_images_from_files(project.id,
-        ImageFileCreateBatch(images=image_list))
+    upload_result = trainer.create_images_from_files(
+        project.id, ImageFileCreateBatch(images=image_list)
+    )
     if not upload_result.is_batch_successful:
         print("Image batch upload failed.")
         for image in upload_result.images:
             print("Image status: ", image.status)
         sys.exit(-1)
 
+
 for inat_species in conf.SPECIES:
     print(f"Tagging species: {inat_species}")
     tag_species(inat_species)
 
-print ("Training...")
+print("Training...")
 iteration = trainer.train_project(project.id)
 while iteration.status != "Completed":
     iteration = trainer.get_iteration(project.id, iteration.id)
-    print ("Training status: " + iteration.status)
-    print ("Waiting 10 seconds...")
+    print("Training status: " + iteration.status)
+    print("Waiting 10 seconds...")
     time.sleep(10)
 
-trainer.publish_iteration(project.id, iteration.id,
-    PUBLISH_ITERATION_NAME, conf.PREDICTION_RESOURCE_ID)
-print ("Done!")
+trainer.publish_iteration(
+    project.id, iteration.id, PUBLISH_ITERATION_NAME, conf.PREDICTION_RESOURCE_ID
+)
+print("Done!")
 
-prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": conf.PREDICTION_KEY})
-predictor = CustomVisionPredictionClient(conf.PREDICTION_ENDPOINT, prediction_credentials)
+prediction_credentials = ApiKeyCredentials(
+    in_headers={"Prediction-key": conf.PREDICTION_KEY}
+)
+predictor = CustomVisionPredictionClient(
+    conf.PREDICTION_ENDPOINT, prediction_credentials
+)
 
-with open(os.path.join (conf.DIR_PATH, "Test/test_image.jpeg"), "rb") as test_image_contents:
+with open(
+    os.path.join(conf.DIR_PATH, "Test/test_image.jpeg"), "rb"
+) as test_image_contents:
     results = predictor.classify_image(
-        project.id, PUBLISH_ITERATION_NAME, test_image_contents.read())
+        project.id, PUBLISH_ITERATION_NAME, test_image_contents.read()
+    )
 
     for prediction in results.predictions:
         print(f"\t{prediction.tag_name}: {prediction.probability * 100:0.2f}%")
--- Setup/Ai_Training/inat_image_downloader.py
+++ Setup/Ai_Training/inat_image_downloader.py
@@ -1,10 +1,10 @@
-'''
+"""
 Código para descargar imágenes por especie, desde una cuenta de inaturalist
 Al descargar las imágenes, se salvan en una carpeta con el nombre de la especie
 y luego se utilizan para alimentar a Customvision.ai de Microsoft
 Nota: Solo se descargan imágenes CC-By
 Mejora: Se puede reutilizar el código para descargar todas las especies vistas en la zona
-'''
+"""
 
 import os
 from pyinaturalist import get_observations
@@ -13,31 +13,40 @@ import conf
 
 
 def download_species(species, inat_user):
-    '''
+    """
     This function receives a species name from inaturalist, and a username, and it
     downloads to the conf.DIR_PATH, all of the available images in original format.
     The downloaded images are then used by other script to train CV models.
-    '''
+    """
     img_dir = os.path.join(conf.DIR_PATH, species)
     if_exist = os.path.exists(img_dir)
     if not if_exist:
         os.makedirs(img_dir)
 
-    observations = get_observations(user_id=inat_user, page='all')
+    observations = get_observations(user_id=inat_user, page="all")
 
-    for obs in observations['results']:
+    for obs in observations["results"]:
         if obs["taxon"]["name"] == species:
             for i in range(0, len(obs["observation_photos"])):
                 if obs["observation_photos"][i]["photo"]["license_code"] == "cc-by":
-                    #print(obs["observation_photos"][i]["photo"])
-                    orig_url = ("https://inaturalist-open-data.s3.amazonaws.com/photos/"
-                        + str(obs["observation_photos"][i]["photo"]["id"]) + "/original.jpeg")
+                    # print(obs["observation_photos"][i]["photo"])
+                    orig_url = (
+                        "https://inaturalist-open-data.s3.amazonaws.com/photos/"
+                        + str(obs["observation_photos"][i]["photo"]["id"])
+                        + "/original.jpeg"
+                    )
                     print("Downloading: ", orig_url)
                     image = requests.get(orig_url, allow_redirects=True)
-                    with open(img_dir + "/" + str(obs["observation_photos"][i]["photo"]["id"])
-                        + '.jpeg', 'wb') as image_file:
+                    with open(
+                        img_dir
+                        + "/"
+                        + str(obs["observation_photos"][i]["photo"]["id"])
+                        + ".jpeg",
+                        "wb",
+                    ) as image_file:
                         image_file.write(image.content)
             print("=" * 91)
+
 
 for inat_species in conf.SPECIES:
     print(f"Downloading species: {inat_species}")
